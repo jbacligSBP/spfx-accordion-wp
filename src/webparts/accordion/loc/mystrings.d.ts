c50b37c44752dc30f78a5764213380eb44e5d78a
Jon Baclig <jbaclig@sbpdiscovery.org>
2017-08-14 16:07:36 -0700

    initialize yo sharepoing generator

---
+++ src/webparts/accordion/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAccordionStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'accordionStrings' {
+  const strings: IAccordionStrings;
+  export = strings;
+}
